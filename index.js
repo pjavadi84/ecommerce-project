87312edd2a998f23ad81b38b1fd372471a51f0ff
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-04-21 21:33:48 -0700

    sending sample get request for testing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi Dear");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hi there yoohoo");
+});
+
+app.listen(3000, () => {
+  console.log("Listening yo");
+});
