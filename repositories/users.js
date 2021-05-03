07b4ccfc41463cf3528535a6128740af70bb684d
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-05-02 22:58:46 -0700

    added json

--- repositories/users.js
+++ repositories/users.js
@@ -1,28 +1,23 @@
 const fs = require("fs");
+
 const crypto = require("crypto");
-const { report } = require("process");
 
 class UsersRepository {
   constructor(filename) {
+    debugger;
     if (!filename) {
-      throw new Error("creating a repository require a filename");
+      throw new Error("Creating a repository requires a filename");
     }
 
     this.filename = filename;
-    // api below check to see if the file exist synchronously. because we only have one
-    // instance of the userRepository we can use accessSync; otherwise, it usually not
-    // recommended to use this way in production mode.
     try {
       fs.accessSync(this.filename);
     } catch (err) {
-      // if the file does not exist, use writeFileSync
       fs.writeFileSync(this.filename, "[]");
     }
   }
 
-  //   READING THE CONTENT OF THE FILENAME:
   async getAll() {
-    // open and read the this.filename and return the parsed value
     return JSON.parse(
       await fs.promises.readFile(this.filename, {
         encoding: "utf8",
@@ -30,19 +25,17 @@ class UsersRepository {
     );
   }
 
-  async create(attributes) {
-    //   generate randomID to be generated and added for each record
-    attributes.id = this.randomId();
-    //   step1: get the records in the filesystem
+  async create(attrs) {
+    attrs.id = this.randomId();
+
     const records = await this.getAll();
-    records.push(attributes);
-    // write an updated records array back to this.filename
+    records.push(attrs);
+
     await this.writeAll(records);
   }
 
-  //   writeAll will JSON stringify the record. second null, 2 args are formatter
-  // to make reading the JSON file more readable
   async writeAll(records) {
+    debugger;
     await fs.promises.writeFile(
       this.filename,
       JSON.stringify(records, null, 2)
@@ -69,7 +62,7 @@ class UsersRepository {
     const record = records.find((record) => record.id === id);
 
     if (!record) {
-      throw new Error(`Record ID ${id} not found`);
+      throw new Error(`Record with id ${id} not found`);
     }
 
     Object.assign(record, attrs);
