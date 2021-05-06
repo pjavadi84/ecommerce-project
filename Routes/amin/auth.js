135b596521e1677916e8efeda185c8cb673b2cba
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-05-06 13:53:31 -0700

    refactor state1a: add folders - ref:Udemy

---
+++ Routes/amin/auth.js
@@ -0,0 +1,82 @@
+const userRepo = require("../../repositories/users");
+
+// Route 1:
+app.get("/signup", (req, res) => {
+  res.send(`
+      <div>
+      Your ID is: ${req.session.userId}
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <input name="passwordConfirmation" placeholder="password confirmation" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    `);
+});
+
+//   Route 2:
+app.post("/signup", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
+
+  // CREATING USER RECORD:
+  //1:  Create a user in our user repo to represent this person
+  const user = await usersRepo.create({ email, password });
+
+  //2: Store the id of that user inside the user cookie using outside library to manage cookie called cookie-session npm
+  req.session.userId = user.id;
+
+  res.send("Account created!!!");
+});
+
+//   Route 3:
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out");
+});
+
+//   Route 4:
+app.get("/signin", (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign in</button>
+      </form>
+    </div>
+    `);
+});
+
+//   ROUTE 5:
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email: email });
+
+  if (!user) {
+    return res.send("Email does not exist");
+  }
+
+  const validPassword = await usersRepo.comparepasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send("invalid password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("You are logged in!");
+});
