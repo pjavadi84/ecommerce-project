d379f4a60c15c0f6d92979937659c900dd74f001
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-05-11 00:38:45 -0700

    activating express-validator library to show validator errors on the database level

--- Routes/amin/auth.js
+++ Routes/amin/auth.js
@@ -1,38 +1,50 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
 const router = express.Router();
 
 // Route 1:
-router.get("/signup", (req, res) => {
+router.get("/signup", async (req, res) => {
   res.send(signupTemplate({ req: req }));
 });
 
 //   Route 2:
-router.post("/signup", async (req, res) => {
-  const { email, passwordEntered, passwordConfirmation } = req.body;
+router.post(
+  "/signup",
+  [
+    check("email").trim().normalizeEmail().isEmail(),
+    check("password").trim().isLength({ min: 4, max: 20 }),
+    check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  const thisEmailExists = await usersRepo.getOneBy({ email });
-  if (thisEmailExists) {
-    return res.send("Email in use");
+    // const { email, passwordEntered, passwordConfirmation } = req.body;
+
+    // const thisEmailExists = await usersRepo.getOneBy({ email });
+    // if (thisEmailExists) {
+    //   return res.send("Email in use");
+    // }
+
+    // if (passwordEntered !== passwordConfirmation) {
+    //   return res.send(
+    //     "both password entered and Password confirmation must match"
+    //   );
+    // }
+
+    // CREATING USER RECORD:
+    //1:  Create a user in our user repo to represent this person
+    // const user = await usersRepo.create({ email, password });
+
+    //2: Store the id of that user inside the user cookie using outside library to manage cookie called cookie-session npm
+    // req.session.userId = user.id;
+
+    // res.send("Account created!!!");
   }
-
-  if (passwordEntered !== passwordConfirmation) {
-    return res.send(
-      "both password entered and Password confirmation must match"
-    );
-  }
-
-  // CREATING USER RECORD:
-  //1:  Create a user in our user repo to represent this person
-  const user = await usersRepo.create({ email, password });
-
-  //2: Store the id of that user inside the user cookie using outside library to manage cookie called cookie-session npm
-  req.session.userId = user.id;
-
-  res.send("Account created!!!");
-});
+);
 
 //   Route 3:
 router.get("/signout", (req, res) => {
