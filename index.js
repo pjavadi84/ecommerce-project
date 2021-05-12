948cf27eaa066eb2db8b01a5fd7ae35fae2055b6
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-05-12 16:36:56 -0700

    added css styling and public css resource

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const authRouter = require("./Routes/amin/auth");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
