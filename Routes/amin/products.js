d0a361bf086d6c114327036776f8b58cc0f89405
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-05-23 19:18:24 -0700

    use Multer for uploading files

--- Routes/amin/products.js
+++ Routes/amin/products.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
-const { validationResult } = require("express-validator");
+
 const router = express.Router();
+const upload = multer({ Storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -11,10 +15,17 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-  res.send("Submitted!");
-});
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    console.log(req.file);
+
+    res.send("Submitted!");
+  }
+);
 
 module.exports = router;
