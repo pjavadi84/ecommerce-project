9347abbcec23f5b352a73d88c3828c6033bea066
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-05-25 00:01:16 -0700

    moved rerror handling logic into a middleware

--- Routes/amin/auth.js
+++ Routes/amin/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
-
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -11,7 +10,6 @@ const {
   requireEmailExists,
   requireValidPasswordForUser,
 } = require("./validators");
-
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
@@ -21,14 +19,8 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
     const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({
       email,
@@ -54,18 +46,11 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
     const { email } = req.body;
-
     const user = await usersRepo.getOneBy({ email });
-
     req.session.userId = user.id;
-
     res.send("You are signed in!!!");
   }
 );
