c7cd66fb9a707aff2f1f466efe9c96eeb09b457b
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-06-13 22:17:13 -0700

    rendering cart items

--- Routes/carts.js
+++ Routes/carts.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
-// Receive a post request to add an item to a cart
+// A) Receive a post request to add an item to a cart or update the existing item quantity
 router.post("/cart/products", async (req, res) => {
   //  Figure out the cart
   let cart;
@@ -16,6 +18,7 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
+  console.log(cart);
   const existingItem = cart.items.find(
     (item) => item.id === req.body.productId
   );
@@ -25,7 +28,10 @@ router.post("/cart/products", async (req, res) => {
     existingItem.quantity++;
   } else {
     // add new product id to items array
-    cart.items.push({ id: req.body.productId, quantity: 1 });
+    cart.items.push({
+      id: req.body.productId,
+      quantity: 1,
+    });
   }
 
   await cartsRepo.update(cart.id, {
@@ -34,8 +40,25 @@ router.post("/cart/products", async (req, res) => {
 
   res.send("Product added to cart");
 });
-// Receive a GET request to show all items in a cart
 
+// B) Receive a GET request to show all items in a cart
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  console.log(cart);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 // Receive a POST request to delete an item from a cart
 
 module.exports = router;
