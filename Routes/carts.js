691a0d99b6a79c363541a86a87730ef17ac7ec47
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-06-13 21:00:03 -0700

    updating the item quantity whehter user add a new item to cart or the existing one to reflect item quantity

--- Routes/carts.js
+++ Routes/carts.js
@@ -16,10 +16,21 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  console.log(cart);
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
   // either increment quantity of the existing product
+  if (existingItem) {
+    // increment quantity and save cart
+    existingItem.quantity++;
+  } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
 
-  // OR add new product to items array
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send("Product added to cart");
 });
