78028b92b1438834fb282870331be60035801cdd
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-06-13 20:51:12 -0700

    aadding cart array to cart json with item ID of the product

--- Routes/carts.js
+++ Routes/carts.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const cartsRepo = require("../repositories/carts");
 
 // Receive a post request to add an item to a cart
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productId);
+router.post("/cart/products", async (req, res) => {
+  //  Figure out the cart
+  let cart;
+  if (!req.session.cartId) {
+    //   We dont have cart, we need to create one
+    //  and store the cart id on the req.session.cartId property
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    //   We have the cart! lets get it from the repo
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+
+  console.log(cart);
+  // either increment quantity of the existing product
+
+  // OR add new product to items array
 
   res.send("Product added to cart");
 });
