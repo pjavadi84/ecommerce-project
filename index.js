92ec03b43ccf9c8212bfd749a38b2aec90a2edcf
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-06-16 16:36:46 -0700

    make it better

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const productsRouter = require("./routes/products.js");
 const cartsRouter = require("./routes/carts.js");
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
@@ -22,6 +23,6 @@ app.use(productsRouter);
 app.use(cartsRouter);
 app.unsubscribe(adminProductsRouter);
 
-app.listen(3000, () => {
-  console.log("Listening");
+app.listen(port, () => {
+  console.log("listening on *:5000");
 });
